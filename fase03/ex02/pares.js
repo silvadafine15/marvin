3bca915edd0de7be7bf5a4dc8e55f347de7945ba
silvadafine15 <57634540+silvadafine15@users.noreply.github.com>
2019-12-04 16:06:24 -0300

    pares 001

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,12 +1,11 @@
-function pares(num1,num2) {
-if (num1 % 2 === 0&&num2 % 2 === 0) {
+function pares(num1, num2){
 
-return("Os números são pares ")
-
+if (num1%2==0&&num2%2==0){
+    return "Os números são pares";
 }
+
 else {
+    return "Os números não são pares";
+    }
 
- return("Os números não são pares")
-
-}
 }
