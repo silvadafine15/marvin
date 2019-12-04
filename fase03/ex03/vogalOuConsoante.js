86abb5c33f7b0e6de360384a579d4cc3069dc901
silvadafine15 <57634540+silvadafine15@users.noreply.github.com>
2019-12-04 14:39:47 -0300

    vogal

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,12 @@
+function vogalOuConsoante(letra){
+
+
+if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'||letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'){
+    return "Vogal";
+}
+
+else {
+    return "Consoante";
+    }
+
+}
